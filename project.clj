7ed32806f73f5dbb7dbbef21223da7bf1ddf3fe6
oakes <zsoakes@gmail.com>
2013-10-05 15:30:08 -0400

    Initial import

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject lein-fruit "0.1.0-SNAPSHOT"
+  :description "Plugin for easy Clojure/iOS development and deployment"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/UNLICENSE"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/fruit.clj
@@ -0,0 +1,6 @@
+(ns leiningen.fruit)
+
+(defn fruit
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
