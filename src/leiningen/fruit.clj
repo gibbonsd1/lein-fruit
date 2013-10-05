7e9ca9404facdca398ef8f7e76e9ce4c0ea3642e
oakes <zsoakes@gmail.com>
2013-10-05 18:51:13 -0400

    Consolidate code for now

--- src/leiningen/fruit.clj
+++ src/leiningen/fruit.clj
@@ -1,13 +1,19 @@
 (ns leiningen.fruit
-  (:refer-clojure :exclude [compile])
-  (:require [leiningen.fruit.compile :as fruit.compile]
+  (:require [leiningen compile javac]
             [robert.hooke :as hooke]))
+
+(defn compile-source
+  "Compiles both Java and Clojure source files."
+  [{java-only :java-only :as project} & args]
+  (apply leiningen.javac/javac project args)
+  (when-not java-only
+    (leiningen.compile/compile project)))
 
 (defn execute-subtask
   "Executes a subtask defined by `name` on the given project."
   [project name args]
   (case name
-    "compile" (fruit.compile/compile project)
+    "compile" (compile-source project)
     :else (println "Subtask is not recognized:" name)))
 
 (defn classpath-hook
