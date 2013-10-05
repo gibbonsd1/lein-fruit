f24ee5c1b3b415f38fe5d9514ba37be78549addd
oakes <zsoakes@gmail.com>
2013-10-05 16:56:19 -0400

    Add compile task

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Plugin for easy Clojure/iOS development and deployment"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
+  :dependencies [[robert/hooke "1.3.0"]]
   :eval-in-leiningen true)
--- src/leiningen/fruit.clj
+++ src/leiningen/fruit.clj
@@ -1,6 +1,24 @@
-(ns leiningen.fruit)
+(ns leiningen.fruit
+  (:refer-clojure :exclude [compile])
+  (:require [leiningen.fruit.compile :as fruit.compile]
+            [robert.hooke :as hooke]))
+
+(defn execute-subtask
+  "Executes a subtask defined by `name` on the given project."
+  [project name args]
+  (case name
+    "compile" (fruit.compile/compile project)
+    :else (println "Subtask is not recognized:" name)))
+
+(defn classpath-hook
+  [f {{:keys [robovm-path]} :ios :as project}]
+  (conj (f project)
+        (str robovm-path "/lib/robovm-rt.jar")
+        (str robovm-path "/lib/robovm-objc.jar")
+        (str robovm-path "/lib/robovm-cocoatouch.jar")))
 
 (defn fruit
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+  "Provides a main entry point."
+  [project & [cmd & args]]
+  (hooke/add-hook #'leiningen.core.classpath/get-classpath #'classpath-hook)
+  (execute-subtask project cmd args))
