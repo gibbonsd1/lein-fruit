3650de61b72deb7acff70a6abbaee30209c5bba0
oakes <zsoakes@gmail.com>
2013-10-05 21:10:41 -0400

    Add several meta tasks similar to lein-droid

--- src/leiningen/fruit.clj
+++ src/leiningen/fruit.clj
@@ -11,21 +11,21 @@
 
 (defn source-to-bytecode
   "Compiles the Java and Clojure source files into bytecode."
-  [{java-only :java-only :as project} args]
-  (apply leiningen.javac/javac project args)
+  [{java-only :java-only :as project}]
+  (leiningen.javac/javac project)
   (when-not java-only
     (leiningen.compile/compile project)))
 
-(defn bytecode-to-native
-  "Compiles the bytecode into native code."
+(defn run-robovm
+  "Runs the RoboVM executable."
   [{{:keys [robovm-path robovm-args]} :ios :as project} args]
   (->> [(str robovm-path robovm-exec)
-        "-arch" "x86" "-os" "ios" "-cp"
+        "-os" "ios" "-cp"
         (->> (for [path robovm-jars] (str robovm-path path))
              (clojure.string/join ":")
              (str (:target-path project) "/classes:"))
-        args
         robovm-args
+        args
         (str (:main project))]
        flatten
        (remove nil?)
@@ -35,8 +35,18 @@
   "Executes a subtask defined by `name` on the given project."
   [project name args]
   (case name
-    "compile" (source-to-bytecode project args)
-    "create-native" (bytecode-to-native project args)
+    "compile" (source-to-bytecode project)
+    "create-x86" (run-robovm project ["-arch" "x86" args])
+    "create-arm" (run-robovm project ["-arch" "thumbv7" args])
+    "ipa" (run-robovm project ["-arch" "thumbv7" "-createipa" args])
+    "run" (run-robovm project ["-arch" "x86" "-run" args])
+    "install" (run-robovm project ["-arch" "thumbv7" "-run" args])
+    "build" (doseq [task ["compile" "create-x86"]]
+              (execute-subtask project task args))
+    "release" (doseq [task ["compile" "ipa"]]
+                (execute-subtask project task args))
+    "doall" (doseq [task ["compile" "run"]]
+              (execute-subtask project task args))
     :else (println "Subtask is not recognized:" name)))
 
 (defn classpath-hook
