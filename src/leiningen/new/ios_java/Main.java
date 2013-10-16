500a8dd7ea50e2f1650d90f330ae68305fa8bf01
oakes <zsoakes@gmail.com>
2013-10-16 19:56:11 -0400

    Finish java template

--- src/leiningen/new/ios_java/Main.java
+++ src/leiningen/new/ios_java/Main.java
@@ -1,16 +1,30 @@
 package {{package}};
 
-import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
-import org.robovm.cocoatouch.foundation.NSDictionary;
-import org.robovm.cocoatouch.uikit.UIApplication;
-import org.robovm.cocoatouch.uikit.UIApplicationDelegate;
+import org.robovm.cocoatouch.coregraphics.*;
+import org.robovm.cocoatouch.foundation.*;
+import org.robovm.cocoatouch.uikit.*;
 
 public class {{class-name}} extends UIApplicationDelegate.Adapter {
+	private UIWindow window = null;
+	private int clickCount = 0;
+	
 	public boolean didFinishLaunching(UIApplication app, NSDictionary opts) {
-		try {
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		final UIButton button = UIButton.fromType(UIButtonType.RoundedRect);
+		button.setFrame(new CGRect(115.0f, 121.0f, 91.0f, 37.0f));
+		button.setTitle("Click me!", UIControlState.Normal);
+		
+		button.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+			@Override
+			public void onTouchUpInside(UIControl control, UIEvent event) {
+				button.setTitle("Click #" + (++clickCount), UIControlState.Normal);
+			}
+		});
+		
+		window = new UIWindow(UIScreen.getMainScreen().getBounds());
+		window.setBackgroundColor(UIColor.lightGrayColor());
+		window.addSubview(button);
+		window.makeKeyAndVisible();
+		
 		return true;
 	}
 
