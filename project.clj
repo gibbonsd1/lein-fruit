e48afb3553098e21d0448ec77819e2d2d2fe88d5
oakes <zsoakes@gmail.com>
2013-10-18 11:34:34 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-fruit "0.1.0-SNAPSHOT"
+(defproject lein-fruit "0.1.0"
   :description "Plugin for easy Clojure/iOS development and deployment"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
