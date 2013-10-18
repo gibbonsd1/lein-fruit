0da4c38a89f0650e7a11a230ca397f3a6d271062
oakes <zsoakes@gmail.com>
2013-10-18 17:59:22 -0400

    Make preview release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-fruit "0.1.0"
+(defproject lein-fruit "0.1.0-preview1"
   :description "Plugin for easy Clojure/iOS development and deployment"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
