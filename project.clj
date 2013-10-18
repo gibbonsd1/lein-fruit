d60b413629345ad16c557f0fd001a25589e26d1c
oakes <zsoakes@gmail.com>
2013-10-18 18:04:53 -0400

    Add :url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-fruit "0.1.0-preview1"
   :description "Plugin for easy Clojure/iOS development and deployment"
+  :url "https://github.com/oakes/lein-fruit"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[robert/hooke "1.3.0"]]
