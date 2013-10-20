35fee6ebcef8e939bced3c6b1b8f62f841aee008
oakes <zsoakes@gmail.com>
2013-10-20 12:04:13 -0400

    Default the version numbers to 0.0.1

--- src/leiningen/new/ios_java/project.clj
+++ src/leiningen/new/ios_java/project.clj
@@ -1,4 +1,4 @@
-(defproject {{app-name}} "0.1.0-SNAPSHOT"
+(defproject {{app-name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :java-source-paths ["src"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
