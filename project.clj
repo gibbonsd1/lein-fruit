c8e9199b68f1b43ac1bd046b413cbc959e561499
Zach Oakes <zsoakes@gmail.com>
2014-04-11 17:21:47 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-fruit "0.1.1"
+(defproject lein-fruit "0.2.0"
   :description "Plugin for easy Clojure/iOS development and deployment"
   :url "https://github.com/oakes/lein-fruit"
   :license {:name "Public Domain"
