14fc20b7c41d58fcd5dc8881bc58e9974114b8cc
oakes <zsoakes@gmail.com>
2014-06-13 17:34:53 -0400

    Fix typo

--- src/leiningen/fruit.clj
+++ src/leiningen/fruit.clj
@@ -61,7 +61,7 @@
     "create-ipa" (run-robovm project ["-arch" "thumbv7" "-createipa" args])
     "run-ipa" (run-robovm project ["-arch" "thumbv7" "-run" args])
     "ipa" (or (execute-subtask project "create-ipa" args)
-              (execute-subtask project "run-ipa") args)
+              (execute-subtask project "run-ipa" args))
     "release" (or (execute-subtask project "compile" args)
                   (execute-subtask project "ipa" args))
     
